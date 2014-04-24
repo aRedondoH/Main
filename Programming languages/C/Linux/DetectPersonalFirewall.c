82de75e39ab56159fae64b84dac6cfb6abc7ae32
Alberto Redondo <albertoredondo@securenok.com>
2014-04-24 14:20:38 +0200

    new method 2 to detect if firewall on/off

--- Programming languages/C/Linux/DetectPersonalFirewall.c
+++ Programming languages/C/Linux/DetectPersonalFirewall.c
@@ -11,47 +11,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
+int read_int(const char* tempFile) {
 
-int read_int(const char* tempFile){
 	// Open file
-	FILE* file = fopen(tempFile, "rb");
-	int i=0;
+	FILE* file = fopen(tempFile, "ab+");
+	// Give file permissions
+	system("chmod 700 temp");
+	int i = 0;
 	// Read int from file
-	fscanf (file, "%d", &i);
+	fscanf(file, "%d", &i);
 	// Close file
 	fclose(file);
+	// Remove temp file
+	system("rm temp");
+
 	return i;
 }
 
-void firewall_is_connected(){
+void firewall_is_connected_method_1() {
 
 	// Make command for Counting lines from drop ports in iptables
-	const char * cmd = "sudo iptables -L -n | wc -l > temp";
+	const char * cmd = "iptables -L -n | wc -l > temp";
+
 	// Run command
 	system(cmd);
-	const char* temp="temp";
+	const char* temp = "temp";
 	int lines = read_int(temp);
 
 	// Check if the Firewall is enabled/disabled
-	if (lines==8){
+	if (lines == 8) {
 		printf("Firewall is disabled\n");
-	}else{
+	} else {
 		printf("Firewall is enabled\n");
 	}
 }
 
+void method_2(){
+	  FILE *fp;
+	  int status;
+	  char path[1035];
+
+	  /* Open the command for reading. */
+	  fp = popen("iptables -L -n | wc -l", "r");
+	  if (fp == NULL) {
+	    printf("Failed to run command\n" );
+	    exit;
+	  }
+
+	  int lines = 0;
+	  while (!feof (fp))
+	     {
+	       fscanf (fp, "%d", &lines);
+	     }
+	  // Check if the Firewall is enabled/disabled
+	  	if (lines == 8) {
+	  		printf("Firewall is disabled\n");
+	  	} else {
+	  		printf("Firewall is enabled\n");
+	  	}
+
+	  /* close */
+	  pclose(fp);
+}
+
 int main(void) {
-
-	// Open file
-		FILE* file = fopen("temp", "ab+");
-	// Give file permissions
-		system("chmod 777 temp");
-
-		printf("we are going to check if the firewall is switch on/off: \n");
-
+	printf("we are going to check if the firewall is switch on/off: \n");
 	// Check if the firewall is on/off
-		firewall_is_connected();
-	// Remove temp file
-	    system("rm temp");
+	for (;;){
+		//firewall_is_connected_method_1();
+		method_2();
+		sleep(1);
+	}
 }
