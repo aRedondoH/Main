961281252c8e426fc135d25a74c7176d0dfcd054
Alberto Redondo <albertoredondo@securenok.com>
2014-05-08 11:36:06 +0200

    Change the way to get open ports

--- Programming languages/C/Windows/auditMain.c
+++ Programming languages/C/Windows/auditMain.c
@@ -52,7 +52,11 @@ void getOpenPorts(){
 	char *next_token;
 	char str1[30];
 	char str2[30];
+	char str3[30];
+	char str4[30];
 	int dontPrintAnyMore = 0;
+	int noPortsOpen = 0;
+	int portOpen = 0;
 
 	/* Get open ports */
 	system("netsh firewall show state > tempFirewallOutput.txt");
@@ -61,23 +65,45 @@ void getOpenPorts(){
 		printf("Error %d when open file", err);
 	}
 	strcpy(str2, "IMPORTANT:");
+	strcpy(str3, "\n");
+	strcpy(str4, "No");
 
 	/* Read the output a line at a time - output it. */
 	while (fgets(portsOutput, 300, fp) != NULL) {		
 		strcpy(portsOutputCopy, portsOutput); // copy of portsOutput
 		token = strtok_s(portsOutputCopy, " ", &next_token);
 		strcpy(str1, token);
-		/* if it find the word "IMPORTANT:" it doesn't print any more*/
-		int ret = strcmp(str1, str2);
+		/* if it find the word "IMPORTANT:" or " " it doesn't print any more*/
+		int ret = strcmp(str1, str2);		
 		if (ret == 0){
 			dontPrintAnyMore = 1;
 		}
-		/* print output for our purpose */
-		if ((count > 10) && (dontPrintAnyMore != 1)){
-			printf("%s", portsOutput);
+		int ret2 = strcmp(str1, str3);
+		if ((count > 13) && (ret2 == 0)){
+			dontPrintAnyMore = 1;
 		}
-		count++;
+		// if it find the word "No" it means there aren't ports open
+		int ret3 = strcmp(str1, str4);
+		if (ret3 == 0){
+			noPortsOpen = 1;
+		}
+		// Get port
+		if ((count > 13) && (noPortsOpen != 1)){
+			sscanf(portsOutput, "%d", &portOpen);
+		}
+
+		/* print output for our purpose */
+		if ((count > 13) && (dontPrintAnyMore != 1) && (noPortsOpen!=1)){
+			printf("Port open: %d\n", portOpen);
+		}
+		
+		count++; // counting lines from the tempFirewallOutput
 	}
+	if (noPortsOpen == 1){
+		printf("No ports are currently open\n");
+	}
+	fclose(fp);
+	//system("del tempFirewallOutput.txt");
 }
 
 void audit(){
@@ -90,4 +116,5 @@ void audit(){
 int __cdecl wmain(__in int argc, __in_ecount(argc) PCWSTR argv[])
 {
 	audit();
+	
 }
