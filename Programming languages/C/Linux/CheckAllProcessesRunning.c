b5ee6fd597fcd8abcfd01264b1bd067f06b08a86
Alberto Redondo <albertoredondo@securenok.com>
2014-05-23 09:40:24 +0200

    Parsing processes to show in right way

--- Programming languages/C/Linux/CheckAllProcessesRunning.c
+++ Programming languages/C/Linux/CheckAllProcessesRunning.c
@@ -11,14 +11,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
+/* Remove blanks from strings */
+char* deblank(char* input) {
+	int i, j;
+	char *output = input;
+	for (i = 0, j = 0; i < strlen(input); i++, j++) {
+		if (input[i] != ' ')
+			output[j] = input[i];
+		else
+			j--;
+	}
+	output[j] = 0;
+	return output;
+}
+
+/* Parsing the processes lines */
+void parsingLine(char line[200]) {
+
+	char* stringToCut;
+
+	// Parsing processes thats begin with '['
+	if (line[1] == '[') {
+		line[1] = ' ';
+		stringToCut = strtok(line, "]");
+		stringToCut = deblank(stringToCut);
+		printf("%s\n", stringToCut);
+	} else if (line[1] == '/') { // Parsing processes thats begin with '/'
+		char *processToPrint;
+		stringToCut = strtok(line, "/");
+		while (stringToCut != NULL ) {
+			processToPrint = stringToCut;
+			stringToCut = strtok(NULL, "/");
+		}
+		printf("%s", processToPrint);
+	} else {
+		// if the process does not need to parsing do nothing
+		printf("%s", deblank(line));
+	}
+
+}
+
 void listEveryProcessRunning() {
 	char line[200];
-	static const char filename[]= "temp.txt";
 
-	/* Get list the processes running */
-		system("ps aux | awk '{printf $11 \" \\n \"}' | uniq -u | head -n -7 | tail -n +2 > temp.txt");
+	FILE *fp;
+	fp =
+			popen(
+					"ps aux | awk '{printf $11 \" \\n \"}' | uniq -u | head -n -7 | tail -n +2",
+					"r");
 
-	FILE *fp=fopen(filename, "r");
 	/* Error open file */
 	if (fp == NULL ) {
 		printf("Failed to run command\n");
@@ -26,23 +69,15 @@ void listEveryProcessRunning() {
 
 	/* Read the output a line at a time -output it. */
 	while (fgets(line, sizeof line, fp) != NULL ) {
-		printf("Process running: %s\n", line);
+		parsingLine(line);
 	}
 
 	/* Close */
 	pclose(fp);
-
-	/* Remove file*/
-	int status = remove(filename);
-	if (status == 0){
-		printf("File %s deleted successfully.\n", filename);
-	}else{
-		printf("Unable to delete the file\n");
-	}
 }
 
 int main(void) {
-	puts("we going to check all the processes running in the machine:\n ");
+	printf("we going to check all the processes running in the machine:\n");
 	listEveryProcessRunning();
 	return EXIT_SUCCESS;
 }
