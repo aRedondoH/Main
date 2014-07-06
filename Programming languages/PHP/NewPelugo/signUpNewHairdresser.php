f9c813f3b203a93ec9a79bced37abc1ba0060905
alberto <alberto@alberto-laptop>
2014-07-06 16:49:10 +0200

    add two function to sign up, one with bind check and another with the opposite behaviour

--- Programming languages/PHP/NewPelugo/signUpNewHairdresser.php
+++ Programming languages/PHP/NewPelugo/signUpNewHairdresser.php
@@ -6,7 +6,33 @@ define("DB_USER", "dbo462711312");
 define("DB_PASSWORD", "arh217956");
 define("DB_DATABASE", "db462711312");
 
-function signUp(){
+function signUpWithoutBind(){
+	
+// Connecting to database 
+        $con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD); 
+// selecting database 
+        mysql_select_db(DB_DATABASE);
+        
+        /* Obtaining arguments from POST variable */
+        $id=null;
+        $nameHairdresser= $_POST['hairdresserName'];
+        $adressToSafe= $_POST['adress'];
+        $cityToSafe= $_POST['city'];
+        $provinceToSafe = $_POST['province'];
+        $timetableToSafe = $_POST['timeTable'];
+        $emailToSafe = $_POST['email'];
+        $phoneToSafe = $_POST['phoneNumber'];
+        
+        /* Query */
+$result = mysql_query("INSERT INTO peluquerias (id_pelu, nombre, direccion, ciudad, provincia, horario, correo, telefono) VALUES ('$id', '$nameHairdresser', '$adressToSafe', '$cityToSafe', '$provinceToSafe', '$timetableToSafe', '$emailToSafe', '$phoneToSafe') ");
+        if($result){
+                        echo "Su peluquería ha sido registrada correctamente";
+        }else{
+                        echo "Ha ocurrido un error durante el registro";
+        }	
+}
+
+function signUpWithBind(){
 
 	// Connecting to database 
 	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD,DB_DATABASE);
@@ -59,7 +85,7 @@ function check(){
 	}else{	
 		/* If the gaps are not empty*/
 		if ( (!empty($_POST['hairdresserName'])) && (!empty($_POST['adress'])) && (!empty($_POST['city'])) && (!empty($_POST['province'])) && (!empty($_POST['timeTable']))  &&(!empty($_POST['email'])) && (!empty($_POST['phoneNumber']))  ){
-				signUp();
+				signUpWithoutBind();
 				
 		}else{
 				echo "Es necesario rellenar todos los campos";
