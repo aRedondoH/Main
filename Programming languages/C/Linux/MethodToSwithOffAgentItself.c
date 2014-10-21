eadfbdded44a545f452aba215edb5ad969c3e487
Alberto Redondo <albertoredondo@securenok.com>
2014-10-21 10:25:29 +0200

    Adding multithreading to check if the Agent needs to switch off

--- Programming languages/C/Linux/MethodToSwithOffAgentItself.c
+++ Programming languages/C/Linux/MethodToSwithOffAgentItself.c
@@ -30,6 +30,9 @@ struct Agent_info {
 struct Agent_info aInfo;
 int p=3;
 
+void *switchOffAgentIfNecessary( void *ptr);
+
+
 /* if Agent needs to switch off return 1
  * if Agent does not need to switch off return 0 */
 int determineIfAgentNeedToSwitchOff(int currentCpuUsage, int maxCpuUsage) {
@@ -104,24 +107,46 @@ void *threadOverLoad(void *arg){
 	return NULL;
 }
 
+void *switchOffAgentIfNecessary( void *ptr){
+	for (;;) {
+			getPidAndCpuFromAgentName(aInfo.agentName);
+
+			if (determineIfAgentNeedToSwitchOff(aInfo.cpuUsage,80)==1){
+				printf("The Agent has anomalous behaviour it will switch off itself \n");
+				exit(1);
+			}
+			sleep(2);
+		}
+	return NULL;
+}
+
 int main(void) {
 	puts("we going to switch off the Agent "); /* prints we going to switch off the Agent  */
+	const char * message1="Thread 1";
+	const char * message2="Thread 2";
+	/* Copy the name of the process to be monitored */
 	strcpy(aInfo.agentName, "./MethodToSwithOffAgentItself");
-	/* Create a thread identifier */
-	pthread_t pth;
 
-	/* Create worker thread */
-	//pthread_create(&pth, NULL, threadOverLoad, void );
-
-	for (;;) {
-		getPidAndCpuFromAgentName(aInfo.agentName);
-
-		if (determineIfAgentNeedToSwitchOff(aInfo.cpuUsage,0)==1){
-			printf("The Agent has anomalous behaviour it will switch off itself \n");
-			exit(1);
-		}
-		sleep(1);
+	/* Run functions in two threads at the same time */
+	pthread_t thread1,thread2;
+	   int rc,rc2;
+	   long t;
+	printf("Running multithreading...\n");
+	rc = pthread_create(&thread1,NULL, switchOffAgentIfNecessary,(void*)message1);
+	if (rc){
+		printf("Error - pthread_create() pthread1 return code: %d\n", rc);
+		exit(EXIT_FAILURE);
 	}
-	pthread_join(pth,NULL);
+	rc2 = pthread_create(&thread2,NULL,threadOverLoad, (void*)message2);
+	if(rc2){
+		printf("Error - pthread_create() pthread2 return code: %d\n", rc2);
+	}
+
+	printf("pthread_create() for thread 1 returns: %d\n", rc);
+	printf("pthread_create() for thread 2 returns: %d\n", rc2);
+	/* Run threads */
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+
 	return EXIT_SUCCESS;
 }
