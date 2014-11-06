d73d60a4160f245f1da2de1be91abc2a5478b1c7
Alberto Redondo <albertoredondo@securenok.com>
2014-11-06 11:48:43 +0100

    Putting device into the dynamic list

--- Programming languages/C/Linux/GetPS2AndSerialDevices.c
+++ Programming languages/C/Linux/GetPS2AndSerialDevices.c
@@ -24,18 +24,19 @@ struct node_device {
 
 /* List Operations */
 /* Append */
-void append(char deviceNameToInsert[MAXLENGHTLINE],char sysfsToInsert[MAXLENGHTLINE]) {
-
+void append(char deviceNameToInsert[MAXLENGHTLINE],
+		char sysfsToInsert[MAXLENGHTLINE]) {
 	/* Allocate memory for the node */
 	struct node_device *right, *temp;
-	temp = (struct node_device *) malloc(sizeof(struct node_devide));
+	/* Allocate memory for the node */
+	temp = (struct node_device *) malloc(sizeof(struct node_device));
 	/* Assign the specific information in fields */
 	/* Assign name of the device */
 	strcpy(temp->deviceName, deviceNameToInsert);
 	/* Assign fisical location */
 	strcpy(temp->Sysfs, sysfsToInsert);
 
-	right = (struct node_rule *) devices;
+	right = (struct node_device *) devices;
 	while (right->next != NULL )
 		right = right->next;
 	right->next = temp;
@@ -44,15 +45,18 @@ void append(char deviceNameToInsert[MAXLENGHTLINE],char sysfsToInsert[MAXLENGHTL
 }
 
 /* Add */
-void add(char deviceNameToInsert[MAXLENGHTLINE], char sysfsToInsert[MAXLENGHTLINE]) {
+void add(char deviceNameToInsert[MAXLENGHTLINE],
+		char sysfsToInsert[MAXLENGHTLINE]) {
 	/* Create a node to fill it with the temporal information */
 	struct node_device *temp;
+	/* Allocate memory for the node */
+	temp = (struct node_device *) malloc (sizeof(struct node_device));
 	/* Assign the specific information in fields */
 	/* Assign name of the device */
 	strcpy(temp->deviceName, deviceNameToInsert);
-	/* Assign fisical location */
+	/* Assign physical location */
 	strcpy(temp->Sysfs, sysfsToInsert);
-	/* Add to rule list */
+	/* Add device to the list */
 	if (devices == NULL ) {
 		devices = temp;
 		devices->next = NULL;
@@ -63,7 +67,8 @@ void add(char deviceNameToInsert[MAXLENGHTLINE], char sysfsToInsert[MAXLENGHTLIN
 }
 
 /* Add in a specific location */
-void addafter (int loc, char deviceNameToInsert[MAXLENGHTLINE], sysfsToInsert[MAXLENGHTLINE]) {
+void addafter(int loc, char deviceNameToInsert[MAXLENGHTLINE],
+		char sysfsToInsert[MAXLENGHTLINE]) {
 	int i;
 	struct node_device *left, *right, *temp;
 	left = NULL;
@@ -72,12 +77,80 @@ void addafter (int loc, char deviceNameToInsert[MAXLENGHTLINE], sysfsToInsert[MA
 	right = devices;
 
 	for (i = 1; i < loc; i++) {
-			left = right;
-			right = right->next;
-		}
+		left = right;
+		right = right->next;
+	}
 
 	/* Allocate memory for the node */
-		temp = (struct node_device *) malloc(sizeof(struct node_rule));
+	temp = (struct node_device *) malloc(sizeof(struct node_device));
+
+	/* Assign the specific information in fields */
+	/* Assign name of the device */
+	strcpy(temp->deviceName, deviceNameToInsert);
+	/* Assign fisical location */
+	strcpy(temp->Sysfs, sysfsToInsert);
+
+	/* add device to the list in a specific location */
+	left->next = temp;
+	left = temp;
+	left->next = right;
+
+}
+
+/* Count element of the list */
+int count() {
+	struct node_device *n;
+	int c = 0;
+	n = devices;
+	while (n != NULL ) {
+		n = n->next;
+		c++;
+	}
+	return c;
+}
+
+/* Insert device */
+void insertDevice(char deviceNameTemp[MAXLENGHTLINE],
+		char sysfsTemp[MAXLENGHTLINE]) {
+
+	int c = 0;
+	struct node_device *temp;
+
+	/* Assign the devices list to temp */
+	temp = devices;
+
+	if (temp == NULL ) {
+		add(deviceNameTemp, sysfsTemp);
+	} else {
+		c = count();
+		if (c == 0)
+			add(deviceNameTemp, sysfsTemp);
+		else if (c < count())
+			addafter(++c, deviceNameTemp, sysfsTemp);
+		else {
+			append(deviceNameTemp, sysfsTemp);
+		}
+	}
+}
+
+/* Display list of devices */
+void displayListOfDevices() {
+	struct node_device *r;
+	r = devices;
+	if (r == NULL ) {
+		printf("The list of devices is empty\n");
+	} else {
+		printf("List of Devices\n");
+		while (r != NULL ) {
+			printf("-----------------------\n");
+			printf("Device Name: %s", r->deviceName);
+			printf("Physical path: %s", r->Sysfs);
+			//printf("-----------------------\n");
+			/* Pointing to the next device in th list */
+			r = r->next;
+			fflush(stdout);
+		}
+	}
 }
 
 void makeListOfPs2AndSerialDevicesConected() {
@@ -92,35 +165,46 @@ void makeListOfPs2AndSerialDevicesConected() {
 		printf("Failed to run command\n");
 	}
 	char * token1;
-	char freeTextName[MAXLENGHTLINE];
+	char deviceNameTemp[MAXLENGHTLINE];
+	char physicalNameTemp[MAXLENGHTLINE];
 
 	/* Read output from the command running */
 	while (fgets(line, sizeof line, fp) != NULL ) {
 		token1 = strtok(line, " ");
 
-		if (strcmp("N:", token1) == 0) {
-			strcpy(freeTextName, "");
-			token1 = strtok(NULL, " ");
-			/* Go to the device name */
-			while (token1 != NULL ) {
-				strcat(freeTextName, token1);
+		if (token1 != NULL ) {
+			if (strcmp("N:", token1) == 0) {
+				strcpy(deviceNameTemp, "");
 				token1 = strtok(NULL, " ");
+				/* Go to the device name */
+				while (token1 != NULL ) {
+					strcat(deviceNameTemp, token1);
+					token1 = strtok(NULL, " ");
+				}
+				//printf("%s", freeTextName);
 			}
-			printf("%s", freeTextName);
 		}
+
 		if (token1 != NULL ) {
 			if (strcmp("S:", token1) == 0) {
 				/* Go to Sysfs */
 				token1 = strtok(NULL, " ");
-				printf("%s", token1);
+				strcpy(physicalNameTemp, token1);
+				//printf("%s", token1);
+				/* Insert device into the list */
+				insertDevice(deviceNameTemp, physicalNameTemp);
 			}
 		}
 	}
+	fclose(fp);
 }
 
 int main(void) {
 	puts("we going to detect PS2 and serial devices");
-	makeListOfPs2AndSerialDevicesConected();
-
-	return EXIT_SUCCESS;
+	//for(;;){
+		makeListOfPs2AndSerialDevicesConected();
+		displayListOfDevices();
+	//}
+	fflush(stdout);
+	return 0;
 }
